4ed87f47d2d39ce5287d6212ecc29298faa8aee0
Me4502 <mnmiller1@me.com>
2015-07-18 13:16:26 +1000

    lowercase

--- src/main/java/com/me4502/ModularFramework/exception/ModuleNotInstantiatedException.java
+++ src/main/java/com/me4502/ModularFramework/exception/ModuleNotInstantiatedException.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.me4502.ModularFramework.exception;
+package com.me4502.modularframework.exception;
 
 public class ModuleNotInstantiatedException extends Exception {
 }
