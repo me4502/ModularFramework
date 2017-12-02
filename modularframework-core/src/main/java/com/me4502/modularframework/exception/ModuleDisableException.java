d834070d493befa7f631a8c20090594bfd5578df
Matthew Miller <mnmiller1@me.com>
2017-12-02 14:09:58 +1000

    Added supressed exceptions to enable/disable to provide more information about what module failed to enable/disable

---
+++ modularframework-core/src/main/java/com/me4502/modularframework/exception/ModuleDisableException.java
@@ -0,0 +1,8 @@
+package com.me4502.modularframework.exception;
+
+public class ModuleDisableException extends Exception {
+
+    public ModuleDisableException(String message) {
+        super(message);
+    }
+}
---
+++ modularframework-core/src/main/java/com/me4502/modularframework/exception/ModuleEnableException.java
@@ -0,0 +1,8 @@
+package com.me4502.modularframework.exception;
+
+public class ModuleEnableException extends Exception {
+
+    public ModuleEnableException(String message) {
+        super(message);
+    }
+}
